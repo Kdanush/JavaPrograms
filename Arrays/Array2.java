6cd375f4782a51ddee08d8fa46a56c1d9ea36647
dhanu <dhanush1682003@gmail.com>
2026-07-06 15:40:03 +0530

    solved

--- Arrays/Array2.java
+++ Arrays/Array2.java
@@ -1,7 +1,26 @@
 package Arrays;
+import java.util.*;
 
 public class Array2 {
     public static void main(String[] args){ 
-    System.out.println("Hii");
+    Scanner sc = new Scanner(System.in);
+    int nums[]=new int[6];
+    boolean isAsending = true;
+    for(int i = 0; i<nums.length ;i++){
+        nums[i]= sc.nextInt();
+    }
+    for(int i =0;i<nums.length-1; i++){
+        if(nums[i]>nums[i+1]){
+            isAsending=false;//condition for decending
+            break;
+        }
+    }
+    if(isAsending){
+        System.out.println("Array is in asending order");
+    }
+    else{
+         System.out.println("Array is not in asending order");
+    }
+    sc.close();
     }
 }
